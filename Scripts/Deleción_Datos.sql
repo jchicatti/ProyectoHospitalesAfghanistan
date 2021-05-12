eb1ce05a52fd8e9ed4905b3fb1caded65b235637
Sebas Dulong <77305951+dulongski@users.noreply.github.com>
2021-05-12 12:21:07 -0500

    delete

---
+++ Scripts/Deleción_Datos.sql
@@ -0,0 +1,4 @@
+truncate protocol, personel, covid_cases, resource_catalog, inventory, update_hospital, action_catalog, 
+personel_vm, problem_catalog, questionnaire_status_catalog , telephone, hospital,district , province 
+
+restart identity;
