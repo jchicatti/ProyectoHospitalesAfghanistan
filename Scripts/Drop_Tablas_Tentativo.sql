d79ab28ce341057acb1c42fa8714f7059e25321a
JoseUlisesQuevedo <47492091+JoseUlisesQuevedo@users.noreply.github.com>
2021-05-12 12:00:02 -0500

    Add files via upload

---
+++ Scripts/Drop_Tablas_Tentativo.sql
@@ -0,0 +1,19 @@
+
+
+drop table covid_cases;
+drop table inventory;
+drop table personel;
+drop table protocol;
+drop table update_hospital;
+drop table action_catalog;
+drop table problem_catalog ;
+drop table resource_catalog ;
+drop table questionnaire_status_catalog ;
+drop table personel_vm;
+drop table telephone ;
+drop table hospital;
+drop table province;
+drop table district;
+drop type funds_enum;
+drop type hospital_type_enum;
+drop type resource_type_enum;
