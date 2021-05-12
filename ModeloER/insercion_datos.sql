6f30f429ad3c88a0adb257b483dc38af50c8e8f4
guillermoNaranjo <70484786+guillermoNaranjo@users.noreply.github.com>
2021-05-12 10:45:12 -0500

    Update insercion_datos.sql

--- ModeloER/insercion_datos.sql
+++ ModeloER/insercion_datos.sql
@@ -1,64 +1,101 @@
 --Populating catalogs
 
-insert into resources_catalog
-(nombre)
+insert into Resource_Catalog
+(product_name, product_type)
 values
-("Oxygen reserves"),
-("Antipyretics (paracetamol)"),
-("Anesthetic drugs/Muscular Relaxant"),
-("Alcohol solutions (>70) and soap for handwashing"),
-("Disposable masks (minimum P2)"),
-("Disposable vinyl gloves"),
-("Disposable hats"),
-("Disposable aprons"),
-("Visors"),
-("Disposable shoe covers"),
-("Covid test kits"),
-("?");
+('Oxygen reserves','general'),
+('Antipyretics (paracetamol)','general'),
+('Anesthetic drugs/Muscular Relaxant','general'),
+('Alcohol solutions (>70) and soap for handwashing','general'),
+('Disposable masks (minimum P2)','general'),
+('Disposable vinyl gloves','general'),
+('Disposable hats','covid'),
+('Disposable aprons','covid'),
+('Visors','covid'),
+('Disposable shoe covers', 'covid'),
+('Covid test kits', 'covid');
 
-insert into action_catalog
-(nombre)
+insert into Action_Catalog
+(action_name)
 values
-("NO ADDITIONAL ACTION NEEDED"),
-("CALL BACK TOMORROW TO COMPLETE DATA"),
-("FIND NEW PHONE NUMBER"),
-("FIND AUTHORIZATION"),
-("OTHER");
+('NO ADDITIONAL ACTION NEEDED'),
+('CALL BACK TOMORROW TO COMPLETE DATA'),
+('FIND NEW PHONE NUMBER'),
+('FIND AUTHORIZATION'),
+('OTHER');
 
 
-insert into problem_catalog
-(nombre)
+insert into Problem_Catalog
+(problem)
 values
-("NO PROBLEM"),
-("SOME DATA IS MISSING"),
-("REFUSED TO SPEAK"),
-("NO ANSWER"),
-("WRONG PHONE NUMBER"),
-("DOCTOR HAS QUIT");
+('NO PROBLEM'),
+('SOME DATA IS MISSING'),
+('REFUSED TO SPEAK'),
+('NO ANSWER'),
+('WRONG PHONE NUMBER'),
+('DOCTOR HAS QUIT');
 
-insert into questionnaire_status_catalog
-(nombre)
+insert into Questionnaire_Status_Catalog
+(status)
 values 
-("QUESTIONNAIRE COMPLETED"),
-("QUESTIONNAIRE COMPLETED PARTIALLY"),
-("QUESTIONNAIRE NOT DONE"),
-("THIS WAS A TEST"),
-("OTHER - SEE COMMENTS");
+('QUESTIONNAIRE COMPLETED'),
+('QUESTIONNAIRE COMPLETED PARTIALLY'),
+('QUESTIONNAIRE NOT DONE'),
+('THIS WAS A TEST'),
+('OTHER - SEE COMMENTS');
 
 -- Agregamos empleados de VoxMap falsos
-insert into personel_vm
-(nombre)
+insert into Personel_VM
+(employee_name)
 values 
-("Armando"),
-("Bruno"),
-("Carla"),
-("Daniela");
+('Armando'),
+('Bruno'),
+('Carla'),
+('Daniela');
+
+
+--PROVINCES
+
+"Badakhshan"
+"Badghis"
+"Baghlan"
+"Balkh"
+"Bamyan"
+"Daykundi"
+"Farah"
+"Faryab"
+"Ghazni"
+"Ghor"
+"Hilmand"
+"Hirat"
+"Jawzjan"
+"Kabul"
+"Kandahar"
+"Kapisa"
+"Khost"
+"Kunar"
+"Kunduz"
+"Laghman"
+"Logar"
+"Maidan Wardak"
+"Nangarhar"
+"Nimroz"
+"Nuristan"
+"Paktika"
+"Paktya"
+"Panjsher"
+"Parwan"
+"Samangan"
+"Sar-e-Pul"
+"Takhar"
+"Uruzgan"
+"Zabul"
 
 -- Agregamos un par de hospitales falsos
 insert into hospital
-(nombre, direccion, latitud, longitud, altitud, distrito, tipo, "MOPH_number")
+(hospital_name, address, latitude, longitude, altitude, district, province, hospital_type, MOPH_number)
 values
-('Angeles','Periferico Sur',100,200,300, 'Magdalena Contreras','distrital',1023),
-('SEDNA','EDOMEX',200,1,3020, 'Naucalpan','distrital',1038),
-('Cardiología','Centro',3,4,5, 'Alvaro Obregón','distrital',93837)
+('Angeles','Periferico Sur',100,200,300, 'Magdalena Contreras','ejemplo','distrital',1023),
+('SEDNA','EDOMEX',200,1,3020, 'Naucalpan','ejemplo','distrital',1038),
+('Cardiologia','Centro',3,4,5, 'Alvaro Obregon','ejemplo','distrital',93837);
 
