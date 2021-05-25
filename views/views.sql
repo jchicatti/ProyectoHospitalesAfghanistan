9aa974255daef5990c4e319ce0e1bc75f99f903e
Sebas Dulong <77305951+dulongski@users.noreply.github.com>
2021-05-25 18:15:43 -0500

    que las tablas solo dependan de updates completos

--- views/views.sql
+++ views/views.sql
@@ -6,11 +6,26 @@ FROM update_hospital uh
   INNER JOIN (
     SELECT uh2.id_hospital, MAX(uh2.update_date) AS most_recent FROM update_hospital uh2 GROUP BY uh2.id_hospital 
   ) ms ON uh.id_hospital = ms.id_hospital AND uh.update_date = most_recent;
+  
+
+-- View del update completo más reciente por hospital
+create view most_recent_complete_update_by_hospital as
+SELECT 
+  uh.id_hospital, uh.update_date, id_update, name_responder, id_personel_vm , id_questionnare_status, id_problem_status , id_action_status , funds, additional_comments
+FROM update_hospital uh 
+  INNER JOIN (
+    SELECT uh2.id_hospital, MAX(uh2.update_date) AS most_recent 
+    FROM update_hospital uh2 
+    where uh2.id_questionnare_status = 1 
+    GROUP BY uh2.id_hospital 
+  ) ms ON uh.id_hospital = ms.id_hospital AND uh.update_date = most_recent
+  
+ drop view most_recent_update_by_hospital cascade
 
 -- View de casos y muertes por provincia
 create view cases_and_deaths_by_province as
 select p.id_province, count(cc.covid_deaths), count(cc.positive_patients) from covid_cases cc 
-join most_recent_update_by_hospital mrubh using (id_update)
+join most_recent_complete_update_by_hospital mrubh using (id_update)
 join hospital h using (id_hospital)
 join province p on h.province = p.id_province 
 group by id_province;
@@ -18,14 +33,14 @@ group by id_province;
 -- View de hospitales con campañas de awareness
 create view hospitals_with_awareness_campaigns as
 select h.id_hospital, p.covid_awareness from hospital h 
-join most_recent_update_by_hospital mrubh using (id_hospital)
+join most_recent_complete_update_by_hospital mrubh using (id_hospital)
 join protocol p using (id_update)
 where p.covid_awareness = true ;
 
 -- View de hospitales y si tienen campaña de awareness ordenados por sus casos
 create view hospitals_awareness_cases as
 select h.id_hospital, p.covid_awareness, cc.positive_patients from hospital h 
-join most_recent_update_by_hospital mrubh using (id_hospital)
+join most_recent_complete_update_by_hospital mrubh using (id_hospital)
 join protocol p using (id_update)
 join covid_cases cc using (id_update)
 order by cc.positive_patients desc ;
@@ -56,7 +71,7 @@ group by pv.id_personel_vm
 create view patient_percentages as (
 with all_patients as(
 select id_hospital, cc.covid_recovered+cc.covid_deaths+cc.positive_patients as patient_no 
-from most_recent_update_by_hospital mrubh 
+from most_recent_complete_update_by_hospital mrubh 
 join covid_cases cc using (id_update))
 select mrubh2.id_hospital, cc2.covid_deaths/all_patients.patient_no as death_percentage, 
 cc2.covid_recovered/all_patients.patient_no as recovered_percentage,
@@ -67,7 +82,7 @@ join covid_cases cc2 );
 -- View de provincias sin capacidad de hacer pruebas
 create view provinces_wout_covid_test_cap as
 select p.id_province , p.province_name, p2.test_capacity 
-from most_recent_update_by_hospital mrubh 
+from most_recent_complete_update_by_hospital mrubh 
 join hospital h using (id_hospital)
 join province p on h.province = p.id_province 
 join protocol p2 using (id_update)
@@ -76,7 +91,7 @@ where exists (select * from protocol p3 where p3.test_capacity = false)
 -- View de distritos sin capacidad de hacer pruebas
 create view districts_wout_covid_test_cap as
 select d.id_district, d.district_name , p2.test_capacity 
-from most_recent_update_by_hospital mrubh 
+from most_recent_complete_update_by_hospital mrubh 
 join hospital h using (id_hospital)
 join district d on h.district = d.id_district 
 join protocol p2 using (id_update)
