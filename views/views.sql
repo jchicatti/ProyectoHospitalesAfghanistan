d619bbab3030a56c4a26e0d7e0753ea872b5ebd9
Sebas Dulong <77305951+dulongski@users.noreply.github.com>
2021-05-25 17:02:15 -0500

    agregue la view de porcentajes de pacientes
    ver comentario de esa view

--- views/views.sql
+++ views/views.sql
@@ -5,7 +5,7 @@ SELECT
 FROM update_hospital uh 
   INNER JOIN (
     SELECT uh2.id_hospital, MAX(uh2.update_date) AS most_recent FROM update_hospital uh2 GROUP BY uh2.id_hospital 
-  ) ms ON uh.id_hospital = ms.id_hospital AND uh.update_date = most_recent
+  ) ms ON uh.id_hospital = ms.id_hospital AND uh.update_date = most_recent;
 
 -- View de casos y muertes por provincia
 create view cases_and_deaths_by_province as
@@ -13,14 +13,14 @@ select p.id_province, count(cc.covid_deaths), count(cc.positive_patients) from c
 join most_recent_update_by_hospital mrubh using (id_update)
 join hospital h using (id_hospital)
 join province p on h.province = p.id_province 
-group by id_province
+group by id_province;
 
 -- View de hospitales con campañas de awareness
 create view hospitals_with_awareness_campaigns as
 select h.id_hospital, p.covid_awareness from hospital h 
 join most_recent_update_by_hospital mrubh using (id_hospital)
 join protocol p using (id_update)
-where p.covid_awareness = true 
+where p.covid_awareness = true ;
 
 -- View de hospitales y si tienen campaña de awareness ordenados por sus casos
 create view hospitals_awareness_cases as
@@ -28,7 +28,7 @@ select h.id_hospital, p.covid_awareness, cc.positive_patients from hospital h
 join most_recent_update_by_hospital mrubh using (id_hospital)
 join protocol p using (id_update)
 join covid_cases cc using (id_update)
-order by cc.positive_patients desc 
+order by cc.positive_patients desc ;
 
 --Vista con el nombre de hospitales que tienen <=1 números de telefono
 create view hospitals_w_few_contacts as (
@@ -43,7 +43,17 @@ create view hospital_telephone as (
 select id_hospital, h.hospital_name, count(t.id_telephone), t.active from hospital h join telephone t using (id_hospital) group by t.active, h.id_hospital order by h.id_hospital
 );
 
-
+-- View de porcentajes de muertos, recuperados y positivos por hospital, no jala si covid cases está vacía porque all patients va a estar vacía
+create view patient_percentages as (
+with all_patients as(
+select id_hospital, cc.covid_recovered+cc.covid_deaths+cc.positive_patients as patient_no 
+from most_recent_update_by_hospital mrubh 
+join covid_cases cc using (id_update))
+select mrubh2.id_hospital, cc2.covid_deaths/all_patients.patient_no as death_percentage, 
+cc2.covid_recovered/all_patients.patient_no as recovered_percentage,
+cc2.positive_patients/all_patients.patient_no as positive_percentage
+from most_recent_update_by_hospital mrubh2 
+join covid_cases cc2 );
 
 -- para probar la primera view (deben salir los días 23 y 24)
 insert into update_hospital 
